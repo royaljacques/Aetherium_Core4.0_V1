4970a8450b90fa02fc8c2d01854f904d4a787d33
royaljacques <maximejacques667@gmail.com>
2021-10-03 02:44:53 +0200

    Ajout des getMonnaie et une fonction sup

--- src/royal/AetheriumCore/commands/admin/economie/AddMonney.php
+++ src/royal/AetheriumCore/commands/admin/economie/AddMonney.php
@@ -1,9 +1,29 @@
-<?phpd
+<?php
 namespace royal\AetheriumCore\commands\admin\economie;
 
 
 use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\lang\Translatable;
+use royal\AetheriumCore\api\MonneyAPI;
+use royal\AetheriumCore\Main;
+use royal\AetheriumCore\utils\Permissions;
 
 class AddMonney extends Command{
+use MonneyAPI;
+    public function __construct(Main $plugin)
+    {
+        parent::__construct("addmoney", "ajouter de la monnaie a un joueur", '', ["/am"]);
+        $this->setPermission(Permissions::MONNAIE);
+    }
 
+    public function execute(CommandSender $sender, string $commandLabel, array $args)
+    {
+        if(!$this->testPermission($sender)){
+            return true;
+        }
+        $player = $sender->getServer()->getPlayerByPrefix($args[0]);
+        $this->addMonnaie($player, $args[0]);
+        return true;
+    }
 }
