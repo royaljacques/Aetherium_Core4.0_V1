dc21b6a7c5e82026107a1a3fa5260a712bb2815c
royaljacquesytb <63215754+royaljacques@users.noreply.github.com>
2021-10-16 23:26:12 +0200

    remise au norme du core du code  de natof
    j'ais changer le nom d'un event par un vrais nom event éxistant

--- src/royal/AetheriumCore/events/DataPacketSendEvent.php
+++ src/royal/AetheriumCore/events/DataPacketSendEvent.php
@@ -3,17 +3,17 @@
 namespace royal\AetheriumCore\events;
 
 use pocketmine\event\Listener;
-use pocketmine\event\server\DataPacketSendEvent;
+use pocketmine\event\server\DataPacketSendEvent as DPSE;
 use pocketmine\network\mcpe\protocol\StartGamePacket;
 use royal\AetheriumCore\api\ItemAPI;
 
-class PacketListenerEvent implements Listener
+class DataPacketSendEvent implements Listener
 {
 
     /** @var array  */
     public array $handlers = [];
 
-    public function onPacketSend(DataPacketSendEvent $event) {
+    public function onPacketSend(DPSE $event) {
         $packets = $event->getPackets();
         $targets = $event->getTargets();
         foreach ($packets as $packet) {
